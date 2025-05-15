2ea54136a4f5988e327a5d1a0bea7b15041bfa02
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-05-16 01:43:55 +0500

    test(tests): :white_check_mark: Add the `newTestServer()` function

--- tests/common_test.go
+++ tests/common_test.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"crypto/sha256"
 	"crypto/sha512"
+	"fmt"
 	"io"
 	"math/big"
 	"net"
 	"net/http"
+	"net/http/httptest"
 	"sync/atomic"
+	"time"
 
 	"github.com/AlekSi/pointer"
 	"github.com/samber/mo"
 	dosProtectorAdapterClients "github.com/thewizardplusplus/go-dos-protector/adapters/clients"
+	dosProtectorAdapterMiddlewares "github.com/thewizardplusplus/go-dos-protector/adapters/middlewares"
 	dosProtectorUsecases "github.com/thewizardplusplus/go-dos-protector/usecases"
 	dosProtectorUsecaseProviders "github.com/thewizardplusplus/go-dos-protector/usecases/providers"
 	powValueTypes "github.com/thewizardplusplus/go-pow/value-types"
@@ -68,4 +72,69 @@ func newTestHTTPClient(
 			}),
 		},
 	)
+}
+
+type middleware func(handler http.Handler) http.Handler
+
+type newTestServerParams struct {
+	leadingZeroBitCountProvider   dosProtectorUsecases.LeadingZeroBitCountProvider
+	resourceProvider              dosProtectorUsecases.ResourceProvider
+	mainSerializedPayloadProvider dosProtectorUsecases.SerializedPayloadProvider
+	randomPayloadByteReader       io.Reader
+	hashProvider                  dosProtectorUsecases.HashProvider
+	gotRequestChannel             chan *http.Request
+	handler                       http.Handler
+	middlewares                   []middleware
+}
+
+func newTestServer(params newTestServerParams) (*httptest.Server, error) {
+	ttl, err := powValueTypes.NewTTL(100 * 365 * 24 * time.Hour)
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct the TTL: %w", err)
+	}
+
+	dosProtectorMiddleware :=
+		dosProtectorAdapterMiddlewares.NewDoSProtectorMiddleware(
+			dosProtectorAdapterMiddlewares.DoSProtectorMiddlewareOptions{
+				DoSProtectorUsecase: dosProtectorUsecases.NewServerDoSProtectorUsecase(
+					dosProtectorUsecases.ServerDoSProtectorUsecaseOptions{
+						LeadingZeroBitCountProvider:   params.leadingZeroBitCountProvider,
+						CreatedAtModulus:              ttl.ToDuration(),
+						TTL:                           ttl,
+						ResourceProvider:              params.resourceProvider,
+						MainSerializedPayloadProvider: params.mainSerializedPayloadProvider,
+						RandomPayloadByteReader:       params.randomPayloadByteReader,
+						RandomPayloadByteCount:        5,
+						HashProvider:                  params.hashProvider,
+						GenerationHashName:            "SHA-256",
+						SecretKey:                     "secret-key",
+						SigningHashName:               "SHA-512",
+					},
+				),
+				HTTPErrorHandler: http.Error,
+			},
+		)
+
+	requestRegisterMiddleware := func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(
+			writer http.ResponseWriter,
+			request *http.Request,
+		) {
+			params.gotRequestChannel <- request.WithContext(context.Background())
+
+			handler.ServeHTTP(writer, request)
+		})
+	}
+
+	middlewares := make([]middleware, 0, len(params.middlewares)+2)
+	middlewares = append(middlewares, dosProtectorMiddleware.ApplyTo)
+	middlewares = append(middlewares, params.middlewares...)
+	middlewares = append(middlewares, requestRegisterMiddleware)
+
+	handler := params.handler
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+
+	return httptest.NewServer(handler), nil
 }
