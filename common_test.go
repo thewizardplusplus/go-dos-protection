eb06dd348ff0b702213812ac80263a45da384d3a
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-05-16 19:40:35 +0500

    docs: :memo: Add the `newExampleServer()` function

--- common_test.go
+++ common_test.go
@@ -4,11 +4,15 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"crypto/sha512"
+	"fmt"
 	"math/big"
 	"net/http"
+	"net/http/httptest"
+	"time"
 
 	"github.com/samber/mo"
 	dosProtectorAdapterClients "github.com/thewizardplusplus/go-dos-protector/adapters/clients"
+	dosProtectorAdapterMiddlewares "github.com/thewizardplusplus/go-dos-protector/adapters/middlewares"
 	dosProtectorUsecases "github.com/thewizardplusplus/go-dos-protector/usecases"
 	dosProtectorUsecaseProviders "github.com/thewizardplusplus/go-dos-protector/usecases/providers"
 	powValueTypes "github.com/thewizardplusplus/go-pow/value-types"
@@ -41,4 +45,55 @@ func newExampleHTTPClient(
 			}),
 		},
 	)
+}
+
+type middleware func(handler http.Handler) http.Handler
+
+type newExampleServerParams struct {
+	leadingZeroBitCountProvider   dosProtectorUsecases.LeadingZeroBitCountProvider
+	resourceProvider              dosProtectorUsecases.ResourceProvider
+	mainSerializedPayloadProvider dosProtectorUsecases.SerializedPayloadProvider
+	hashProvider                  dosProtectorUsecases.HashProvider
+	handler                       http.Handler
+	middlewares                   []middleware
+}
+
+func newExampleServer(params newExampleServerParams) (*httptest.Server, error) {
+	ttl, err := powValueTypes.NewTTL(10 * time.Minute)
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct the TTL: %w", err)
+	}
+
+	dosProtectorMiddleware :=
+		dosProtectorAdapterMiddlewares.NewDoSProtectorMiddleware(
+			dosProtectorAdapterMiddlewares.DoSProtectorMiddlewareOptions{
+				DoSProtectorUsecase: dosProtectorUsecases.NewServerDoSProtectorUsecase(
+					dosProtectorUsecases.ServerDoSProtectorUsecaseOptions{
+						LeadingZeroBitCountProvider:   params.leadingZeroBitCountProvider,
+						CreatedAtModulus:              ttl.ToDuration(),
+						TTL:                           ttl,
+						ResourceProvider:              params.resourceProvider,
+						MainSerializedPayloadProvider: params.mainSerializedPayloadProvider,
+						RandomPayloadByteReader:       rand.Reader,
+						RandomPayloadByteCount:        128,
+						HashProvider:                  params.hashProvider,
+						GenerationHashName:            "SHA-256",
+						SecretKey:                     "secret-key",
+						SigningHashName:               "SHA-512",
+					},
+				),
+				HTTPErrorHandler: http.Error,
+			},
+		)
+
+	middlewares := make([]middleware, 0, len(params.middlewares)+1)
+	middlewares = append(middlewares, dosProtectorMiddleware.ApplyTo)
+	middlewares = append(middlewares, params.middlewares...)
+
+	handler := params.handler
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+
+	return httptest.NewServer(handler), nil
 }
